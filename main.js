46ab272d3f3b3cc17200915ce6584def20fb87de
Nikhil Kumar Gupta <147592489+Maximalord@users.noreply.github.com>
2024-03-17 12:14:58 +0530

    deployed static website with blob storage and mapped it to a custom domain using Azure CDN

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded',(event) => {
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounter2410.azurewebsites.net/api/GetResumeCounter?code=glTp8FbgShTHGVI7_d9ue18rPHm-zsR30-M9k_w8GDFaAzFuXnGNPg=='
+const localfunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         console.log("Website called function API.");
